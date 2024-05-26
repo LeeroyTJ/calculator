301f6d5b5785173ddf7e19aaffd5fabf058a0a7b
LeeroyTJ <jumbethabol@gmail.com>
2024-05-26 13:03:49 +0200

    added functionalities to populate the display when a button is clicked

--- script.js
+++ script.js
@@ -26,11 +26,60 @@ function operate(op, num1, num2) {
         case '-':
             subtract(num1, num2);
             break;   
-        case '*':
+        case '×':
             multiply(num1, num2);
             break;
-        case '/':
+        case '÷':
             divide(num1, num2);
             break;         
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const display = document.querySelector('.display');
+    let displayValue = '';
+
+    const buttons = document.querySelectorAll('.btn');
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            const key = this.getAttribute('data-key');
+
+            switch(key) {
+                case '0':
+                case '1':
+                case '2':  
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '.':
+                    displayValue += key;
+                    break;
+                case 'AC':
+                    displayValue = '';
+                    break;
+                case '←':
+                    displayValue = displayValue.slice(0,-1);
+                    break;
+                case '%':
+                case '÷':
+                case '×':
+                case '-':
+                case '+':
+                    displayValue += ` ${key} `;
+                    break;  
+                case '=':
+                    //perform calculations
+
+                    break;
+                default:
+                    break;    
+            }
+
+            display.textContent = displayValue || '0';
+        });
+    });
+});
